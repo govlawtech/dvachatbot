bd027ab4ca13977d2816f6b34fff65844cefcb2f
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2017-06-25 09:10:57 +1000

    wip

---
+++ DVAESABot/Domain/FactSheet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Microsoft.Azure.Search.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.Search;
+using System.Linq;
+
+namespace SearchExperiment
+{
+    [SerializePropertyNamesAsCamelCase]
+    public class FactSheet
+    {
+        
+        [Key]
+        public string Key { get; set; }
+
+        public string FactsheetId { get; set; }
+
+        [IsSearchable]
+        public string Purpose { get; set; }
+             
+    
+
+    }
+}
---
+++ DVAESABot/Search/FactSheetSearchClient.cs
@@ -0,0 +1,44 @@
+﻿using DVAESABot.Domain;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DVAESABot.Search
+{
+    class FactSheetSearchClient
+    {
+        private readonly SearchIndexClient _searchIndexClient;
+
+        public FactSheetSearchClient()
+        {
+            string searchApiKey = ConfigurationManager.AppSettings["SearchServiceQueryApiKey"];  // ok to be public
+            string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+            string searchIndexName = ConfigurationManager.AppSettings["SearchIndexName"];
+            _searchIndexClient = new SearchIndexClient(searchServiceName, searchServiceName,
+                new SearchCredentials(searchApiKey));
+                            
+        }
+
+
+        public async Task<IList<string>> GetTopMatchingFactsheetIds(string searchQuery, int topNumber)
+        {
+            SearchParameters searchParameters = new SearchParameters()
+            {
+                SearchMode = SearchMode.Any,
+                QueryType = QueryType.Simple,
+                Top = topNumber
+            };
+
+            _searchIndexClient.Documents.Search<FactSheet>(searchQuery,)
+
+            //_searchIndexClient.
+        }
+        
+
+    }
+}
