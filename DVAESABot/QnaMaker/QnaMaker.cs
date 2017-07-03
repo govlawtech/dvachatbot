7286aef0d8d6b3ec8e911b61f4401bdbb35d9ad8
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2017-07-03 11:34:02 +1000

    remove filtering by score on QnA session

--- DVAESABot/QnaMaker/QnaMaker.cs
+++ DVAESABot/QnaMaker/QnaMaker.cs
@@ -1,22 +1,20 @@
-﻿using Microsoft.ApplicationInsights;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Newtonsoft.Json;
 
 namespace DVAESABot.QnAMaker
 {
-
     public class QnaMakerKb
     {
+        private readonly TelemetryClient _telemetry;
         private readonly Uri qnaBaseUri = new Uri("https://westus.api.cognitive.microsoft.com/qnamaker/v1.0");
 
         private readonly string SubscriptionKey = ConfigurationManager.AppSettings["QnaSubscriptionKey"];
 
-        private readonly TelemetryClient _telemetry;
-        
         public QnaMakerKb()
         {
             _telemetry = new TelemetryClient();
@@ -40,26 +38,26 @@ namespace DVAESABot.QnAMaker
             using (var client = new WebClient())
             {
                 client.Headers.Add("Content-Type", "application/json");
-                client.Headers.Add("Ocp-Apim-Subscription-Key", this.SubscriptionKey);
+                client.Headers.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
 
-                 responseString = await client.UploadStringTaskAsync(uri, postBody);
+                responseString = await client.UploadStringTaskAsync(uri, postBody);
             }
 
             var result = ConvertResponseFromJson(responseString);
 
-            if (result == null || result.Score <= 0)
+            if (result == null)
             {
                 _telemetry.TrackTrace("QandAQuestionAnswerFound", new Dictionary<string, string>
-            {
-                {"Question", question }
-            });
+                {
+                    {"Question", question}
+                });
                 return null;
             }
 
             _telemetry.TrackTrace("QandAQuestionAnswerFound", new Dictionary<string, string>
             {
-                {"Question", question },
-                {"Answer", result.Answer }
+                {"Question", question},
+                {"Answer", result.Answer}
             });
 
             return result;
@@ -79,8 +77,5 @@ namespace DVAESABot.QnAMaker
 
             return response;
         }
-
-
-
     }
 }
