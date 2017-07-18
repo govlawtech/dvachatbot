597355da971b225a64780d75ac62995db721875f
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2017-07-18 17:42:29 +1000

    user type dialog smoothed off

--- DVAESABot/Dialogs/UserTypeDialog.cs
+++ DVAESABot/Dialogs/UserTypeDialog.cs
@@ -6,28 +6,61 @@ using System.Web;
 using DVAESABot.Domain;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
+using User = Microsoft.VisualBasic.ApplicationServices.User;
 
 namespace DVAESABot.Dialogs
 {
     public class UserTypeDialog : IDialog<UserType>
     {
+        private static readonly string MEMBER = "Member";
+        private static readonly string DEPENDENT = "Dependent on a member";
+        private static readonly string DEPENDENT_ON_DECEASED = "Dependent on a deceased member";
+        private static readonly string ORGANISATION = "Organisation";
+        private static readonly string OTHER = "Other";
+
         public async Task StartAsync(IDialogContext context)
         {
-
-            List<UserType> choices = Enum.GetValues(typeof(UserType)).Cast<UserType>().ToList();
             PromptDialog.Choice(
-                context,
-                ResponseReceived,
-                choices,
-                "Which best describes you?",
-                "Try again",
-                3
+               
+                context: context,
+                resume: ResponseReceived,
+                choices: new Dictionary<string,IEnumerable<string>>()
+                {
+                    {MEMBER,new [] {"former member","cadet","reservist","peacekeeper","veteran","cadet","mariner","eligible civilian", "permanent force","adf"} },
+                    {DEPENDENT, new List<string>() },
+                    {DEPENDENT_ON_DECEASED, new List<string>()},
+                    {ORGANISATION, new List<string>() },
+                    {OTHER, new List<string>() }
+                },
+                prompt: "Which best describes your circumstances?",
+                retry: "Try again:",
+                attempts: 1
             );
         }
 
-        private async Task ResponseReceived(IDialogContext context, IAwaitable<UserType> activity)
+        private async Task ResponseReceived(IDialogContext context, IAwaitable<string> activity)
         {
-            var userType = await activity;
+            string userTypeString = null;
+            try
+            {
+                userTypeString = await activity;
+            }
+            catch (TooManyAttemptsException e)
+            {
+                await context.SayAsync("Pick one.");
+                await StartAsync(context);
+            }
+
+            UserType userType = userTypeString == MEMBER
+                ? UserType.Member
+                : userTypeString == DEPENDENT
+                    ? UserType.DependentOnMember
+                    : userTypeString == DEPENDENT_ON_DECEASED
+                        ? UserType.DependentOnDeceasedMember
+                        : userTypeString == ORGANISATION
+                            ? UserType.Organisation
+                            : UserType.Other;
+            
             var cc = context.GetChatContextOrDefault();
             cc.User.UserType = userType;
             context.SetChatContext(cc);
